fe362792110cd9f72c5bfa23a6951e7bf5297e3f
vuurball <oneindigedroom@gmail.com>
2018-02-18 14:31:05 +0200

    example of communication between 2 contracts

--- solidity_lang.sol
+++ solidity_lang.sol
@@ -79,3 +79,45 @@ contract CustodialContract {
         _;
     }
 }
+
+/***** interacting with other deployed contracts on the same blockchain ************/
+//CONTRACT 1
+pragma solidity ^0.4.4;
+
+contract ScoreStoreContract {
+	mapping(string => int) PersonScores;
+
+	function addPersonScore(string name, int startingScore)
+	{
+		if(PersonScores[name] > 0)
+		{
+			throw;
+		}
+
+		PersonScores[name] = startingScore;
+	}
+
+	function getScore(string name) returns (int) 
+	{
+		return PersonScores[name];
+	}
+}
+
+//CONTRACT 2 will use CONTRACT 1
+pragma solidity ^0.4.4;
+
+//create an interface tot he 1st contract
+contract IScoreStoreContract{
+    //set a signatrure func to the function we are going to use from contract 1 (same name, params,returns)
+	function getScore(string name) returns (int) ;
+}
+
+contract MyGameContract {
+    //wrapper func to call a function from contract 1
+	function showScore(string name) returns (int) {
+        //create an instance to the interface, pass the addressHash of contract 1 (output when was deployed)
+		IScoreStoreContract scoreStore = IScoreStoreContract(0x8DDbe991C6D2787841248232D494b9846fD44B2C);
+        //call the function in contract 1 and return value.
+		return scoreStore.getScore(name);
+	}
+}
